c680ee69fb1437cfc9f97c4c03df664eb1bee947
Bradhyan <143909229+Bradhyan@users.noreply.github.com>
2024-07-23 23:58:51 -0500

    Add files via upload

---
+++ clase 1-3/ej1.js
@@ -0,0 +1,27 @@
+console.log("hola mundo...");
+//Definición de variables 
+var nombre ="fulano de tal";
+//let: la variable que defina con let no puede ser redefinida con let
+//Las variables definidas con let solo funcionan si estan en bloque y nace y muere en el bloque
+l
+
+
+ let nombre1="Peranito de jesus";
+ console.log(nombre);
+//utilizamos let cuando no queremos que la variable no sea modificada, en cambio var es peligroso por que se pueden redefinir en cualquier parte del codigo, ya que te mostrara el ultimo contenido modificado.
+
+let salary =3200000;
+let state =true; 
+if (salary >4000000){
+    let nombre="Ryan Dah1"
+    console.log(nombre+ "No tiene derecho a subsidio")
+}else{
+    console.log(nombre+ "tiene derecho a subsidio");
+
+}
+//Tenemos definidas dos variables con nombre, solo que la de var es global, y la de let esta en bloque, por eso funcionan simultaneamente y no generan error 
+
+//son variables fijas que no se pueden cambiar y se declaran en mayusculas
+
+const   IVA=19/100;
+//IVA= 15/100;
